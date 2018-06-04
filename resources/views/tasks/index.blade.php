5f519b37e5675f569a2e8ae45737894783111a6b
EC2 Default User <ec2-user@ip-172-31-22-167.us-east-2.compute.internal>
2018-06-04 03:14:00 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Tasklist</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+               <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+                   {!! link_to_route('tasks.create', '新規Taskの投稿') !!}
+            @endforeach
+        </ul>
+    @endif
+
+
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のTask詳細ページ</h1>
+
+    <p>{{ $task->content }}</p>
+
+{!! link_to_route('tasks.edit', 'このTaskを編集', ['id' => $task->id]) !!}
+
+{!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+    {!! Form::submit('削除') !!}
+{!! Form::close() !!}
+
+@endsection
